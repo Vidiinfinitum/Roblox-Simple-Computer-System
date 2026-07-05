56b0056783b2d50a8d9c281e97c231f94fa031f9
Saymon Luna <saymonpersonal2004@gmail.com>
2026-07-04 21:37:17 -0300

    Create CustomizationData.lua

---
+++ Data/CustomizationData.lua
@@ -0,0 +1,112 @@
+local CustomizationData = {
+	
+	UserIcons = {
+		BuildersClub = {
+			Key = "BuildersClub",
+			Name = "Builders Club",
+			Id = "rbxassetid://99360152209480",
+		},
+		
+		BloxyCola = {
+			Key = "BloxyCola",
+			Name = "Bloxy Cola",
+			Id = "rbxassetid://127943751696640",
+		},
+		
+		Cheezburger = {
+			Key = "Cheezburger",
+			Name = "Cheezburger",
+			Id = "rbxassetid://134564048308892",
+		},
+		
+		Tix = {
+			Key = "Tix",
+			Name = "Tix",
+			Id = "rbxassetid://133932065972986",
+		},
+		
+		Taco = {
+			Key = "Taco",
+			Name = "Taco",
+			Id = "rbxassetid://102060134796955",
+		},
+	},
+	
+	Wallpapers = {
+		Default = {
+			Key = "Default",
+			Name = "Default",
+			Id = "rbxassetid://86264140209694",
+		},
+		
+		Headquarters = {
+			Key = "Headquarters",
+			Name = "Headquarters",
+			Id = "rbxassetid://96544395973774",
+		},
+		
+		Robloxcity = {
+			Key = "Robloxcity",
+			Name = "Robloxcity",
+			Id = "rbxassetid://101029962666978",
+		},
+		
+		OldCrossroads = {
+			Key = "OldCrossroads",
+			Name = "Old Crossroads",
+			Id = "rbxassetid://92126613790512",
+		},
+		
+		TownOfRobloxia = {
+			Key = "TownOfRobloxia",
+			Name = "Town of Robloxia",
+			Id = "rbxassetid://100438987320682",
+		},
+		
+		Glasshouses = {
+			Key = "Glasshouses",
+			Name = "Glasshouses",
+			Id = "rbxassetid://102588310610592",
+		},
+		
+		Mansion = {
+			Key = "Mansion",
+			Name = "Mansion",
+			Id = "rbxassetid://111047047307050",
+		},
+		
+		Brickbattle = {
+			Key = "Brickbattle",
+			Name = "Brickbattle",
+			Id = "rbxassetid://105460951652712"
+		},
+		
+		SwordFight = {
+			Key = "SwordFight",
+			Name = "Sword Fight",
+			Id = "rbxassetid://79754250692521"
+		},
+		
+		House = {
+			Key = "House",
+			Name = "Somewhere House",
+			Id = "rbxassetid://79995777864772"
+		},
+	},
+	
+	Musics = {
+		ClairDeLune = {
+			Key = "ClairDeLune",
+			Name = "Clair de Lune",
+			Id = "rbxassetid://1838457617",
+		},
+		
+		Silence = {
+			Key = "Silence",
+			Name = "The Silence",
+			Id = "",
+		}
+	}
+}
+
+return CustomizationData
