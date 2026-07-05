85451c6450707b741e4d95ab0786b0d11c437202
Saymon Luna <saymonpersonal2004@gmail.com>
2026-07-04 21:38:41 -0300

    Create ChaptersData.lua

---
+++ Data/ChaptersData.lua
@@ -0,0 +1,49 @@
+local ChaptersData = {
+	
+	Tutorial = {
+		Order = 1,
+		Key = "Tutorial",
+		Name = "Tutorial",
+		Title = "The tutorial",
+		ImageId = "rbxassetid://111047047307050",
+		PlaceId = 79523467407376,
+	},
+	
+	Chapter1 = {
+		Order = 2,
+		Key = "Chapter1",
+		Name = "Chapter 1",
+		Title = "",
+		ImageId = "rbxassetid://100438987320682",
+		PlaceId = 0,
+	},
+	
+	Chapter2 = {
+		Order = 3,
+		Key = "Chapter2",
+		Name = "Chapter 2",
+		Title = "",
+		ImageId = "rbxassetid://100438987320682",
+		PlaceId = 0,
+	},
+	
+	Chapter3 = {
+		Order = 4,
+		Key = "Chapter3",
+		Name = "Chapter 3",
+		Title = "",
+		ImageId = "rbxassetid://100438987320682",
+		PlaceId = 0,
+	},
+	
+	Chapter4 = {
+		Order = 5,
+		Key = "Chapter4",
+		Name = "Coming Soon",
+		Title = "",
+		ImageId = "rbxassetid://100438987320682",
+		PlaceId = 0,
+	},
+}
+
+return ChaptersData
